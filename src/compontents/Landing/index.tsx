5d90158d85040fc7371b84015d2cc0b2998e8313
xiangzhihai <rickyxiangzhihai@gmail.com>
2021-01-06 17:31:51 -0800

    Routes Done

---
+++ src/compontents/Landing/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const foo = () => (
+  <div>
+    <h1>Land</h1>
+  </div>
+);
+
+export default foo;
---
+++ src/compontents/SignIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const foo = () => (
+  <div>
+    <h1>foo</h1>
+  </div>
+);
+
+export default foo;
